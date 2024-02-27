437632139ad50ee97c98f6e879c680b3fe2cec3c
empty-object <computingstuff93@gmail.com>
2024-02-27 11:59:43 +0000

    Created all project files inside new folder, also got connectiontest working

--- Prototyping/connectiontest.py
+++ Prototyping/connectiontest.py
@@ -1,10 +1,23 @@
 import openrouteservice
-import urllib.request
+from openrouteservice.directions import directions
+import requests
+import json
+import sys
 
-connection=True
-while connection==True:
-    try:
-        urllib.request.urlopen("https://openrouteservice.org")
-        print("Connected")
-    except urllib.error.URLError:
-        print("Connection lost")
+coords = ((-1.5662230584012848, 54.99494634402549),(-1.5808882542425002, 54.99437627680842))
+
+try:
+    client = openrouteservice.Client(key='5b3ce3597851110001cf6248991939055b9748d3b3fefcf616ea5a79')
+    routes = directions(client, coords, profile='foot-walking')
+except requests.exceptions.ConnectionError:
+    print("No internet connection, please try again")
+    sys.exit()
+
+
+dump=json.dumps(routes)
+parsed=json.loads(dump)
+
+
+for item in range(0, len(parsed["routes"][0]["segments"][0]["steps"])):
+    instruction=parsed["routes"][0]["segments"][0]["steps"][item]["instruction"]
+    print(instruction)
