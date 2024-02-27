a86dc1b076968011d409373d3667cdceb8fc1826
empty-object <computingstuff93@gmail.com>
2024-02-27 09:26:43 +0000

    Added all geocode test files, able to access suggested locations from user input

---
+++ Prototyping/geocodetest.py
@@ -0,0 +1,15 @@
+import openrouteservice
+from openrouteservice.geocode import pelias_autocomplete
+import json
+
+
+client = openrouteservice.Client(key='5b3ce3597851110001cf6248991939055b9748d3b3fefcf616ea5a79')
+
+inp=input("Location: ")
+geocode=openrouteservice.geocode.pelias_search(client, inp, focus_point=None, rect_min_x=None, rect_min_y=None, rect_max_x=None, rect_max_y=None, circle_point=None, circle_radius=None, country="GBR", size=None)
+
+dump=json.dumps(geocode)
+parsed=json.loads(dump)
+
+for item in range(0, (len(parsed)-2)):
+    print(parsed["features"][item]["properties"]["label"])
--- Prototyping/routetest.py
+++ Prototyping/routetest.py
@@ -3,7 +3,6 @@
 
 import openrouteservice
 from openrouteservice.directions import directions
-from openrouteservice.places import places
 import json
 
 coords = ((-1.5662230584012848, 54.99494634402549),(-1.5808882542425002, 54.99437627680842))
@@ -19,5 +18,3 @@ parsed=json.loads(dump)
 for item in range(0, len(parsed["routes"][0]["segments"][0]["steps"])):
     instruction=parsed["routes"][0]["segments"][0]["steps"][item]["instruction"]
     print(instruction)
-
-openrouteservice.places.places(client, 'pois', geojson=routes)
