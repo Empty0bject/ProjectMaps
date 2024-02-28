e879874f19e847a0d50b4d933ed07f6abc9a5010
empty-object <computingstuff93@gmail.com>
2024-02-28 13:23:29 +0000

    Trialing a new method for accessing page layout by using text files storing layout details

--- Project/LayoutApp.py
+++ Project/LayoutApp.py
@@ -1,10 +1,15 @@
 import kivy
 from kivy.app import App
 from kivy.uix.label import Label
-    
+
+def read_file():
+    f = open("homepage_layout.txt", "r")
+    print(f.read())
+
 class plot_layout(App):
     def build(self):
-        lbl=Label(text ="test")
-        return lbl
-    #def homepage(self):
-        #return self.build.lbl()
+        lbl=Label()
+    def homepage(self):
+        return self.build.lbl(text="test")
+    
+read_file()
