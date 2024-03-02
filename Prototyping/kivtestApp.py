35c2645e175c6bf9868dd60d7fc18ae0c6f33531
empty-object <computingstuff93@gmail.com>
2024-03-02 11:16:57 +0000

    Scrapped modifying code and instead starting to design a basic layout using BoxLayout

---
+++ Prototyping/kivtestApp.py
@@ -0,0 +1,12 @@
+import kivy
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.uix.boxlayout import BoxLayout
+
+class MainWidget(BoxLayout):
+    pass
+
+class kivtestApp(App):
+    pass
+
+kivtestApp().run()
