a8b617602cd1272e7d13643911853b4a02704ca6
empty-object <computingstuff93@gmail.com>
2024-03-02 11:58:08 +0000

    Created an example of all the kivy widgets required and added OpenDyslexia font pack

--- Prototyping/kivtestApp.py
+++ Prototyping/kivtestApp.py
@@ -2,11 +2,23 @@ import kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
+from kivy.properties import StringProperty
 
-class MainWidget(BoxLayout):
+class Homepage(BoxLayout):
+    count=0
+    my_text=StringProperty("0")
+    text_input_str=StringProperty("Enter text")
+    def on_button_click(self):
+        self.count+=1
+        self.my_text= str(self.count)
+    def on_text_validate(self, widget):
+        self.text_input_str=widget.text
+        
+
+class Settings(BoxLayout):
     pass
 
-class kivtestApp(App):
+class KivtestApp(App):
     pass
 
-kivtestApp().run()
+KivtestApp().run()
