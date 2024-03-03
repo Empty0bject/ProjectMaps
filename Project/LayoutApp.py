d09208aac946b285b9e9d8db065fee67a8964dbb
empty-object <computingstuff93@gmail.com>
2024-03-03 16:53:43 +0000

    Begining to integrate the RoutePlan geocoding into the layout

--- Project/LayoutApp.py
+++ Project/LayoutApp.py
@@ -3,17 +3,26 @@ from kivy.uix.widget import Widget
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import StringProperty
+from RoutePlan import RoutePl
 
 class Homepage(Screen):
     pass
 
-class Settings(Screen):
+class Settings_Page(Screen):
     pass
 
-class RoutePlan(Screen):
-    pass
+class RoutePlan_Page(Screen):
+    startingloc_info= ""
+    endingloc_info= ""
+    RoutePl=RoutePl()
+    def on_text_val_s(self, widget):
+        self.startingloc_info=widget.text
+    def on_text_val_d(self, widget):
+        self.endingloc_info=widget.text
+    def on_plan_press(self, widget):
+        self.RoutePl.GeoLoc(self.startingloc_info, self.endingloc_info)
 
-class RouteJor(Screen):
+class RouteJor_Page(Screen):
     pass
 
 class WindowManager(ScreenManager):
@@ -24,5 +33,3 @@ kv=Builder.load_file('layout.kv')
 class RunApp(App):
     def build(self):
         return kv
-    
-RunApp().run()
--- Project/RoutePlan.py
+++ Project/RoutePlan.py
@@ -1,1 +1,12 @@
-#RoutePlan
+import openrouteservice
+from openrouteservice.geocode import pelias_autocomplete
+import json
+
+class RoutePl():
+    def __init__(self):
+        self.client = openrouteservice.Client(key='5b3ce3597851110001cf6248991939055b9748d3b3fefcf616ea5a79')
+    def GeoLoc(self, startloc, desloc):
+        startcoord=openrouteservice.geocode.pelias_search(self.client, startloc, focus_point=None, rect_min_x=None, rect_min_y=None, rect_max_x=None, rect_max_y=None, circle_point=None, circle_radius=None, country="GBR", size=None)
+        endcoord=openrouteservice.geocode.pelias_search(self.client, desloc, focus_point=None, rect_min_x=None, rect_min_y=None, rect_max_x=None, rect_max_y=None, circle_point=None, circle_radius=None, country="GBR", size=None)
+        dump=json.dumps(geocode)
+        parsed=json.loads(dump)
