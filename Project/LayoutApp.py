538697c6dead67281aa7752bec9f39315ac34cbc
empty-object <computingstuff93@gmail.com>
2024-03-05 15:20:07 +0000

    Beggining to impliment settings

--- Project/LayoutApp.py
+++ Project/LayoutApp.py
@@ -4,24 +4,32 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.properties import StringProperty
 from RoutePlan import RoutePl
+from settings import Settings
 
 class Homepage(Screen):
     pass
 
 class Settings_Page(Screen):
-    pass
+    Settings=Settings()
+    def on_font_press(self, selection):
+        settings_store[0]=self.Settings.Font(selection)
 
 class RoutePlan_Page(Screen):
+    font_selection=settings_store[0]
     RoutePl=RoutePl()
 
+    #accesses the address info input by the user
     def on_text_val_s(self, widget):
         self.startingloc_info=widget.text
     def on_text_val_d(self, widget):
         self.endingloc_info=widget.text
-
+    
     def on_plan_press(self, widget):
+        #returns address information
         loc=self.RoutePl.GeoLoc(self.startingloc_info, self.endingloc_info)
+        #returns the starting and ending coordinates of the addresses
         startcoords, descoords=self.RoutePl.GeoLoc(self.startingloc_info, self.endingloc_info)
+        #returns the steps for the directions in an array format
         global steps
         steps=self.RoutePl.RouteSteps(startcoords, descoords)
 
@@ -32,7 +40,6 @@ class RouteJor_Page(Screen):
     except:
         steps=[""]
     print(steps)
-    
     current_step=StringProperty(steps[0])
     def step_press(self, change):
         if self.count<=-2:
@@ -41,7 +48,6 @@ class RouteJor_Page(Screen):
             try:
                 self.count+=change
                 self.current_step=str(steps[self.count])
-
             except:
                 pass
         else:
@@ -51,6 +57,8 @@ class RouteJor_Page(Screen):
 class WindowManager(ScreenManager):
     pass
 
+global settings_store
+settings_store=["standard"]
 kv=Builder.load_file('layout.kv')
 
 class RunApp(App):
--- Project/settings.py
+++ Project/settings.py
@@ -1,3 +1,10 @@
 class Settings():
     def __init__(self):
         pass
+    def Font(self, selection):
+        font_selection={
+            "standard" :  "Roboto/Roboto-Regular.ttf",
+            "dyslexic" : "OpenDyslexic2/OpenDyslexic-Regular.otf"
+        }
+        print("\n\n\n\n\n\n\n", font_selection[selection], "\n\n\n\n\n\n\n\n\n")
+        return font_selection[selection]
